fc7998a901abb4e7366c8504d4bced74648282e5
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:44:48 -0600

    add solution

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # Follow the instructions in the tab to the right
 # Write your code below
+words = []
+for i in range(5):
+  user_input = input('Enter a word: ')
+  words.append(user_input)
 
+longest = ""
+for word in words:
+  if len(word) > len(longest):
+    longest = word
+
+print(longest)
