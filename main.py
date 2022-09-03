0a0609fa23db6ee094a1446df1efd8d052c5360e
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:44:45 -0600

    add solution

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# See the Instructions tab to the right
 # Find the average of the numbers in this list
 nums = [-34, -13, 28, -34, 17, 43, -11, -25, 16, -35, 129, 30, 120, 10, 40, -5, 51, 32, 134, 36, 81, 87, 26, 49, 67, 36, 137, 29, 108, 58, 30]
+
+total = 0
+for num in nums:
+  total += num
+
+average = total / len(nums)
+
+print(average)
