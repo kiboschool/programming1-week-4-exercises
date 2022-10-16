1b3c2fcfb88bd81b8bdaea04570a32b42faa8278
Rob Cobb <rwcobbjr@gmail.com>
2022-10-16 17:03:50 +0000

    fix module cleanup

--- test_main.py
+++ test_main.py
@@ -9,22 +9,28 @@ class Test(TestCase):
     @patch('builtins.print')
     def test_output(self, mock_print, mock_input):
         import main
-        mock_print.assert_called_with("eeeee")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("eeeee")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.input', side_effect=["bb", "eeeee", "dddd", "ffffff", "a"])
     @patch('builtins.print')
     def test_shuffled_order(self, mock_print, mock_input):
         import main
-        mock_print.assert_called_with("ffffff")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("ffffff")
+        finally:
+            sys.modules.pop('main')
 
     @patch('builtins.input', side_effect=["Giraffe", "Hippo", "Lion", "Dog", "Cat"])
     @patch('builtins.print')
     def test_animals(self, mock_print, mock_input):
         import main
-        mock_print.assert_called_with("Giraffe")
-        sys.modules.pop('main')
+        try:
+            mock_print.assert_called_with("Giraffe")
+        finally:
+            sys.modules.pop('main')
 
 if __name__ == '__main__':
     unittest.main()
